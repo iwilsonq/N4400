5c4f15d0b6ce1ffc506f83dfa1895c6d681d5144
nadajackal <you@example.com>
2015-10-11 21:04:18 -0700

    AI experimentation

---
+++ AnimatedSprite/AnimatedSprite/Animation.hpp
@@ -0,0 +1,33 @@
+//
+//  Animation.h
+//  AnimatedSprite
+//
+//  Created by Ian Wilson on 9/26/15.
+//  Copyright (c) 2015 ia. All rights reserved.
+//
+
+
+#ifndef ANIMATION_INCLUDE
+#define ANIMATION_INCLUDE
+
+#include <vector>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+class Animation
+{
+public:
+    Animation();
+    
+    void addFrame(sf::IntRect rect);
+    void setSpriteSheet(const sf::Texture& texture);
+    const sf::Texture* getSpriteSheet() const;
+    std::size_t getSize() const;
+    const sf::IntRect& getFrame(std::size_t n) const;
+    
+private:
+    std::vector<sf::IntRect> m_frames;
+    const sf::Texture* m_texture;
+};
+
+#endif // ANIMATION_INCLUDE
